f48d964944325de46436a152b9e7fc4b6d18ebfe
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-03-10 20:30:42 -0500

    diagetic audio added

--- John Lemon/Assets/Scripts/PlayerMovement.cs
+++ John Lemon/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,14 @@ public class PlayerMovement : MonoBehaviour {
     public float turnSpeed = 20f;
     Quaternion m_Rotation = Quaternion.identity;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+
 
     // Start is called before the first frame update
     void Start() {
         m_Animator = GetComponent<Animator> ();
         m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,16 @@ public class PlayerMovement : MonoBehaviour {
 
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
+
+         if (isWalking) {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else {
+            m_AudioSource.Stop ();
+        }
     }
 
     void OnAnimatorMove () {
