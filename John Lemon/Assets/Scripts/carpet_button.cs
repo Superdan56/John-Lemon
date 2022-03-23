a06dc6aa2d7ec583dcc82c13a953d834ad876f5e
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-03-23 19:43:36 -0400

    feature 3 finished

--- John Lemon/Assets/Scripts/carpet_button.cs
+++ John Lemon/Assets/Scripts/carpet_button.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class carpet_button : MonoBehaviour
 {
+
+    private int collisionCounter;
+    private bool buttonPressed;
+    AudioSource m_AudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        collisionCounter = 0;
+        buttonPressed = false;
+    }
+
+    void FixedUpdate() {
+        if(collisionCounter >= 1) {
+            buttonPressed = true;
+        }
+        else if(collisionCounter = 0) {
+            buttonPressed = false;
+        }
+
+        if(buttonPressed) {
+            if (!m_AudioSource.isPlaying) {
+                m_AudioSource.Play();
+            }
+        }
     }
 
     // Update is called once per frame
     void OnCollisionEnter(Collider other) {
-        
+        collisionCounter += 1;
     }
 
     void OnCollisionExit(Collider other) {
-        
+        collisionCounter -= 1;
     }
 }
