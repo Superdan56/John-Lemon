201cde8ef8b3fa620293e3a58bf4f35076752b66
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-03-23 19:12:12 -0400

    feature 2 pt 1

---
+++ John Lemon/Assets/Scripts/carpet_button.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carpet_button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(collider other) {
+        
+    }
+
+    void OnCollisionExit(collider other) {
+        
+    }
+}
