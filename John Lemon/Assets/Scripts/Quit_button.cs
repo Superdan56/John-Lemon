d323719ca952d6efcd4a077f65d4c3e5a57a0c52
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-04-05 20:12:06 -0400

    buttons activated, tittle finished

---
+++ John Lemon/Assets/Scripts/Quit_button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit_button : MonoBehaviour
+{
+    public void ButtonPressed() {
+        
+       Application.Quit();
+    }
+}
